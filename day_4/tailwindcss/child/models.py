0fa006235ec3aa0e9f81bcca864f751b34a47c8d
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-06-15 16:05:55 +0500

    Add: relationships added

--- day_4/tailwindcss/child/models.py
+++ day_4/tailwindcss/child/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class ChaiVariety(models.Model):
@@ -18,3 +19,37 @@ class ChaiVariety(models.Model):
     
     def __str__(self):
         return self.name
+    
+    
+# ONE TO MANY!
+class ChaiReview(models.Model):
+    chai = models.ForeignKey(ChaiVariety, on_delete=models.CASCADE,related_name='reviews')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    comment = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    
+    def __str__(self):
+        return f'{self.user.username} review for {self.chai.name}'
+    
+    
+# MANY TO MANY!
+class Store(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    chai_varieties = models.ManyToManyField(ChaiVariety,related_name="stores")
+    
+    def __str__(self):
+        return f'{self.name}'
+   
+# ONE TO ONE!
+class ChaiCertificate(models.Model):
+    chai = models.OneToOneField(ChaiVariety, on_delete=models.CASCADE,related_name='certificate')
+    certificate_number = models.CharField(max_length=100)
+    issue_date = models.DateTimeField(auto_now_add=True)
+    valid_until = models.DateTimeField()
+    
+    def __str__(self):
+        return f'Certificate for {self.chai.name}'
+        
